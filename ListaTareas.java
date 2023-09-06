4184b2f72a9fab7520c68e248fdd81f45199147f
3rick0 <143622327+3rick0@users.noreply.github.com>
2023-09-06 08:39:20 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variable1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTareas {
+    
+    public static void main(String[] args) {
+        
+        String tarea1 = "lavar ropa";
+        String tarea2 = "limpiar";
+        String tarea3 = "barrer";
+        String tarea4 = "cocinar";
+        String tarea5 = "tareadelaescuela";
+        
+        String cadenaTexto = "cadena de texto!!! % $$$";
+        
+        System.out.println("la primera tarea es " + tarea1);
+        System.out.println("la segunda tarea es" + tarea2);
+        System.out.println("la tercera tarea es" + tarea3);
+        System.out.println("la cuarta tarea es" + tarea4);
+        System.out.println("la quinbta tarea es" + tarea5);
+    }
+    
+}
