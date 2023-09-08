1f758fa98a5121975c951d11835073eebb0aa872
3rick0 <143622327+3rick0@users.noreply.github.com>
2023-09-08 08:44:59 -0600

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.aratriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo {
+
+    public static void main(String[] args) {
+        //numeros enteros
+        //numeros decimales
+        int b = 10;
+        int A = 6;
+        int area = (b*A)/2;
+
+
+        System.out.println("el resultado es: "+ area);        
+    }
+    }
+    
+
